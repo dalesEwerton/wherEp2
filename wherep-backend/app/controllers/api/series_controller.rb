87a676d85f0607f37b6b519dd96f0691ea489d6c
dalesEwerton <dales.fragoso@ccc.ufcg.edu.br>
2018-03-19 13:50:34 -0300

    End the series controller

--- wherep-backend/app/controllers/api/series_controller.rb
+++ wherep-backend/app/controllers/api/series_controller.rb
@@ -7,5 +7,53 @@ module Api
                     message: 'Series loaded',
                     data: series},status: :ok
     end
+
+    def show
+      serie = Serie.find(params[ :id])
+      render json: {status: 'SUCCESS',
+                    message: 'Serie loaded',
+                    data: serie}, status: :ok
+    end
+
+    def create
+      serie = Serie.new(serie_params)
+      if serie.save
+        render json: {status: 'SUCCESS',
+                      message: 'Serie Saved',
+                      data: serie}, status: :ok
+      else
+        render json: {status: 'ERROR',
+                      message: 'Serie not saved',
+                      data: serie.erros}, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      serie = Serie.find(params[ :id])
+      serie.destroy
+      render json: {status: 'SUCCESS',
+                    message: 'Serie Deleted',
+                    data: serie}, status: :ok
+    end
+
+    def update
+      serie = Serie.find(params[ :id])
+      if serie.update_attributes(serie_params)
+        render json: {status: 'SUCCESS',
+                      message: 'Serie updated',
+                      data: serie}, status: :ok
+      else
+        render json: {status: 'ERROR',
+                      message: 'Serie note updated',
+                      data: serie.erros},status: :unprocessable_entity
+      end
+    end
+
+
+    private
+
+    def serie_params()
+      params.permit(:title, :imdbId)
+    end
   end
 end
